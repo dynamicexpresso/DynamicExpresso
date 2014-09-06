c38455ca5003962bb6627c719ee29cb3704e53c4
Davide Icardi <davide.icardi@gmail.com>
2014-09-07 01:11:09 +0200

    Added missing file ReferenceType.cs

---
+++ src/DynamicExpresso.Core/ReferenceType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DynamicExpresso
+{
+	public class ReferenceType
+	{
+		public Type Type { get; private set; }
+
+		/// <summary>
+		/// Public name that must be used in the expression.
+		/// </summary>
+		public string Name { get; private set; }
+
+		public ReferenceType(string name, Type type)
+		{
+			if (string.IsNullOrWhiteSpace(name))
+				throw new ArgumentNullException("name");
+
+			if (type == null)
+				throw new ArgumentNullException("type");
+
+			Type = type;
+			Name = name;
+		}
+
+		public ReferenceType(Type type)
+		{
+			if (type == null)
+				throw new ArgumentNullException("type");
+
+			Type = type;
+			Name = type.Name;
+		}
+	}
+}
