3dc87dfc5d99cb9bd03c25fdc538746a85b0cf31
Davide Icardi <davide.icardi@gmail.com>
2014-11-08 19:43:04 +0100

    Version 1.1

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("C# expression interpreter/evaluator. See https://github.com/davideicardi/DynamicExpresso.")]
 [assembly: AssemblyCopyright("Copyright © Davide Icardi 2014")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 
