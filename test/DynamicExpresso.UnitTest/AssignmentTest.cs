6fef0df2188a6f86bfc062b3c3cc1db9e2a84e61
Kéven Barrigas-Bernard <keven.barrigas-bernard@chrc-ccdp.gc.ca>
2020-01-22 08:32:23 -0500

    Added AssignmentTest

---
+++ test/DynamicExpresso.UnitTest/AssignmentTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace DynamicExpresso.UnitTest {
+   public class BoxedValue {
+      private BoxedValue(object value)
+      {
+         Value = value;
+      }
+
+      public object Value { get; }
+      public static implicit operator BoxedValue(string other) => new BoxedValue(other);
+      public static implicit operator BoxedValue(int other) => new BoxedValue(other);
+   }
+
+   public class Container {
+      // ReSharper disable once UnusedAutoPropertyAccessor.Global
+      public BoxedValue BoxedValue { get; set; }
+   }
+
+   [TestFixture]
+   public class AssignmentTest {
+      [Test]
+      public void ImplicitOperatorAssignment()
+      {
+         var container = new Container();
+
+         var interpreter = new Interpreter();
+         interpreter.SetVariable("Container", container);
+         
+         interpreter.Eval("Container.BoxedValue = \"Test\"");
+         Assert.AreEqual("Test", container.BoxedValue.Value);
+         
+         interpreter.Eval("Container.BoxedValue = 2");
+         Assert.AreEqual(2, container.BoxedValue.Value);
+      }
+   }
+}
