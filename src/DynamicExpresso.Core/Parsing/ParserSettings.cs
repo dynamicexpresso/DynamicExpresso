72e4a72b72a7c2705d36e00bf981c0f84ad80a82
metoule <m520152112e@gmail.com>
2021-12-10 09:46:53 +0100

    Implement the Clone method via a copy constructor.

--- src/DynamicExpresso.Core/Parsing/ParserSettings.cs
+++ src/DynamicExpresso.Core/Parsing/ParserSettings.cs
@@ -6,11 +6,11 @@ namespace DynamicExpresso.Parsing
 {
 	internal class ParserSettings
 	{
-		private Dictionary<string, Identifier> _identifiers;
-		private Dictionary<string, ReferenceType> _knownTypes;
-		private HashSet<MethodInfo> _extensionMethods;
+		private readonly Dictionary<string, Identifier> _identifiers;
+		private readonly Dictionary<string, ReferenceType> _knownTypes;
+		private readonly HashSet<MethodInfo> _extensionMethods;
 
-		public ParserSettings(bool caseInsensitive,bool lateBindObject)
+		public ParserSettings(bool caseInsensitive, bool lateBindObject)
 		{
 			CaseInsensitive = caseInsensitive;
 
@@ -27,23 +27,30 @@ namespace DynamicExpresso.Parsing
 			_extensionMethods = new HashSet<MethodInfo>();
 
 			AssignmentOperators = AssignmentOperators.All;
-      
+
 			DefaultNumberType = DefaultNumberType.Default;
-      
+
 			LambdaExpressions = false;
+		}
+
+		private ParserSettings(ParserSettings other) : this(other.CaseInsensitive, other.LateBindObject)
+		{
+			_knownTypes = new Dictionary<string, ReferenceType>(other._knownTypes);
+			_identifiers = new Dictionary<string, Identifier>(other._identifiers);
+			_extensionMethods = new HashSet<MethodInfo>(other._extensionMethods);
+
+			AssignmentOperators = other.AssignmentOperators;
+			DefaultNumberType = other.DefaultNumberType;
+			LambdaExpressions = other.LambdaExpressions;
 		}
 
 		/// <summary>
 		/// Creates a deep copy of the current settings, so that the identifiers/types/methods can be changed
 		/// without impacting the existing settings.
 		/// </summary>
-		internal ParserSettings Clone()
+		public ParserSettings Clone()
 		{
-			var clone = (ParserSettings)MemberwiseClone();
-			clone._identifiers = new Dictionary<string, Identifier>(_identifiers);
-			clone._knownTypes = new Dictionary<string, ReferenceType>(_knownTypes);
-			clone._extensionMethods = new HashSet<MethodInfo>(_extensionMethods);
-			return clone;
+			return new ParserSettings(this);
 		}
 
 		public IDictionary<string, ReferenceType> KnownTypes
@@ -64,31 +71,27 @@ namespace DynamicExpresso.Parsing
 		public bool CaseInsensitive
 		{
 			get;
-			private set;
 		}
 
 		public bool LateBindObject
 		{
 			get;
-			private set;
+		}
+
+		public StringComparison KeyComparison
+		{
+			get;
+		}
+
+		public IEqualityComparer<string> KeyComparer
+		{
+			get;
 		}
 
 		public DefaultNumberType DefaultNumberType
 		{
 			get;
 			set;
-		}
-
-		public StringComparison KeyComparison
-		{
-			get;
-			private set;
-		}
-
-		public IEqualityComparer<string> KeyComparer
-		{
-			get;
-			private set;
 		}
 
 		public AssignmentOperators AssignmentOperators
