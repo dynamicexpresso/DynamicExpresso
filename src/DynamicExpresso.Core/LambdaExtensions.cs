07a7feb76412604aa908045bad3d757a835760ce
eugenealeykin <ealeykin@gmail.com>
2021-12-12 13:38:43 +0200

    separation of concerns

---
+++ src/DynamicExpresso.Core/LambdaExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DynamicExpresso
+{
+	/// <summary>
+	/// Lambda extensions.
+	/// </summary>
+	public static class LambdaExtensions
+	{
+		/// <summary>
+		/// Compiles lambda with declared parameters.
+		/// </summary>
+		public static Delegate Compile(this ParseResult parseResult)
+		{
+			var lambdaExpression = Expression.Lambda(parseResult.Expression, parseResult.DeclaredParameters.ToArray());
+
+			return lambdaExpression.Compile();
+		}
+
+		public static TDelegate Compile<TDelegate>(this ParseResult parseResult)
+		{
+			var lambdaExpression = Expression.Lambda<TDelegate>(parseResult.Expression, parseResult.DeclaredParameters.ToArray());
+
+			return lambdaExpression.Compile();
+		}
+	}
+}
