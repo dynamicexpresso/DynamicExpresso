a83fa24e6ef98a4c52228a0bdd9e845aa124695c
eugenealeykin <ealeykin@gmail.com>
2022-03-20 22:24:52 +0700

    implicit `this` reference #232. https://github.com/dynamicexpresso/DynamicExpresso/issues/232

--- test/DynamicExpresso.UnitTest/IdentifiersTest.cs
+++ test/DynamicExpresso.UnitTest/IdentifiersTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace DynamicExpresso.UnitTest
@@ -37,6 +38,39 @@ namespace DynamicExpresso.UnitTest
 			Assert.AreEqual(2, lambda.Identifiers.Count());
 			Assert.AreEqual("x", lambda.Identifiers.ElementAt(0).Name);
 			Assert.AreEqual("true", lambda.Identifiers.ElementAt(1).Name);
+		}
+
+		[Test]
+		public void This_identifier_variable()
+		{
+			const string Name = "John";
+			var context = new KeyValuePair<string, string>(nameof(Name), Name);
+
+			var interpreter = new Interpreter();
+
+			interpreter.SetVariable("this", context);
+
+			Assert.AreEqual(Name, interpreter.Eval("this.Value"));
+			Assert.AreEqual(nameof(Name), interpreter.Eval("this.Key"));
+
+			Assert.AreEqual(Name, interpreter.Eval("Value"));
+			Assert.AreEqual(nameof(Name), interpreter.Eval("Key"));
+		}
+
+		[Test]
+		public void This_identifier_parameter()
+		{
+			const string Name = "John";
+			var context = new KeyValuePair<string, string>(nameof(Name), Name);
+			var parameter = new Parameter("this", context.GetType());
+
+			var interpreter = new Interpreter();
+
+			Assert.AreEqual(Name, interpreter.Parse("this.Value", parameter).Invoke(context));
+			Assert.AreEqual(nameof(Name), interpreter.Parse("this.Key", parameter).Invoke(context));
+
+			Assert.AreEqual(Name, interpreter.Parse("Value", parameter).Invoke(context));
+			Assert.AreEqual(nameof(Name), interpreter.Parse("Key", parameter).Invoke(context));
 		}
 	}
 }
