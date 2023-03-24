bd65d5cc2c9550c8110e4bbc48fe1027a6020e9e
Davide Icardi <davide.icardi@gmail.com>
2023-03-24 15:09:55 +0100

    fix: nuget package directory

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("Davide Icardi")]
+[assembly: AssemblyCompany("Dynamic Expresso")]
 [assembly: AssemblyProduct("Dynamic Expresso")]
-[assembly: AssemblyDescription("C# expression interpreter/evaluator. See https://github.com/davideicardi/DynamicExpresso.")]
-[assembly: AssemblyCopyright("Copyright © Davide Icardi 2015")]
+[assembly: AssemblyDescription("C# expression interpreter/evaluator. See https://github.com/dynamicexpresso/DynamicExpresso.")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.3.1.0")]
 
